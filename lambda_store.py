f4e41e55f8ef13995c7f40abd10832bbb2978a3e
Anton Pleshivtsev <anton@emby.ru>
2015-09-01 14:16:45 +0300

    initial

---
+++ lambda_store.py
@@ -0,0 +1,38 @@
+import sublime, sublime_plugin
+import urllib
+import json
+
+settings = None
+
+LIST_URL = '{}/list'
+FUNC_URL = '{}/func/'
+
+def resp(url):
+    try:
+        data = urllib.request.urlopen(url).read().decode("utf8")
+        return json.loads(data)
+    except Exception:
+        return {}
+
+class LambdaStoreInsertCommand(sublime_plugin.TextCommand):
+    def run(self, edit, to_insert=""):
+        self.view.insert(edit, self.view.sel()[0].begin(), to_insert)
+
+class LambdaStoreCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        methods_list = resp(LIST_URL.format(settings.get('endpoint'))).get('functions', [])
+        self.view.window().show_quick_panel(methods_list, lambda x:self.on_select(x))
+    def on_select(self, index):
+        if index < 0:
+            return
+        func_source = resp(FUNC_URL.format(settings.get('endpoint')) + methods_list[index]).get('func', '')
+        self.view.run_command("lambda_store_insert", {'to_insert': func_source})
+
+def plugin_loaded():
+    global settings
+    settings = sublime.load_settings('lambda_store.sublime-settings')
+
+if int(sublime.version()) < 3000:
+    plugin_loaded()
+
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Lambda Store
+
+Lambda Store is a Sublime Text 2 & 3 plugin for quick and simple access to a huge library to useful functions (check out [functions repository](https://github.com/allaud/lambda_store))
+
+Lambda Store allows development using functions, not frameworks or libraries.
+
+## Documentation
+
+After installing Lambda Store, use following shortcuts:
+
+**Windows/Linux**
+
+  * Alt-Shift-K to open fuzzy search for function
+
+**OSX**
+
+  * Command-Shift-K to open fuzzy search for function
